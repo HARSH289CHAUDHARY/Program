88bcf247e55bcbcbe1d088d9bcae98d1bd990274
HARSH289CHAUDHARY <114799920+HARSH289CHAUDHARY@users.noreply.github.com>
2023-01-01 22:21:45 +0530

    Add files via upload

---
+++ PROGRAM.py
@@ -0,0 +1,6 @@
+salary = int(input("Enter your salary = "))
+year = int(input("Enter your year of service = "))
+if year>5:
+   print("Your Net bonus amount is",salary+1000)
+else :
+   print("your net bonus amount is",salary+0)
